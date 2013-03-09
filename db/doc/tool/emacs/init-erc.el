8335f7ea37bf5243b1464a2923f5361f97d9d5f4
sw2wolf <czsq888@163.com>
2013-03-09 10:50:00 +0800

    qqq

--- db/doc/tool/emacs/init-erc.el
+++ db/doc/tool/emacs/init-erc.el
@@ -10,7 +10,7 @@
 		   erc-port "6667"
 
            erc-autojoin-channels-alist 
-		   '(("freenode.net" "#openbsd" "#stumpwm" "#erlang" "#lisp" "#haskell")
+		   '(("freenode.net" "#openbsd" "#stumpwm" "#erlang" "#lisp" "#xmonad")
 			 ("oftc.net" "#emacs-cn"))
 
            erc-keywords '("lisp" "racket" "haskell")
