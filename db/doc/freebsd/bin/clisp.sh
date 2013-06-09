06fd4333d7383ef9d603dfa38c980bc2a3fb81c6
sw2wolf <czsq888@163.com>
2013-06-09 17:29:09 +0800

    misc.

--- db/doc/freebsd/bin/clisp.sh
+++ db/doc/freebsd/bin/clisp.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-res=$(clisp -q -q -modern -ansi -x "$*")
+#expr=`echo ""| dmenu -p Eval: -b -nb '#000000' -nf '#FFFFFF' -fn '-*-simsun-medium-r-normal-*-16-*-*-*-*-*-iso10646-1'`
+expr=`zenity --width 350 --entry --text "Please input a CL expression" \
+\(stopLoss\) \(div618\) \(his\) \(win-ssq\) \(hit-ssq\)`
 
 if [ $? -eq 0 ]
 then
+res=$(clisp -norc -q -q -i $MD/money/util.lisp -x "(cd \"$MD/money\") $expr")
 zenity --info --text="$res"
-else
-zenity --error --text="$res"
 fi
