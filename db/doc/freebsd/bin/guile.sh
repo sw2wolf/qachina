f9c6664cb5da431c4c78f0637d3316c315886509
sw2wolf <czsq888@163.com>
2014-01-21 16:56:52 +0800

    misc.

--- db/doc/freebsd/bin/guile.sh
+++ db/doc/freebsd/bin/guile.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-expr=`zenity --width 350 --entry --text "Please input a expression" \
-\(div618\) \(stopLoss\) \(his\) \(win-ssq\) \(hit-ssq\)`
+if test $# -eq 1; then
+	guile -q -l $MD/scheme/money-guile.scm -c "$1"
+	exit 0
+fi
+
+expr=`zenity --width 350 --entry --text "Please input a CL expression" \
+\(\) \(apropos\ \"\"\) \(m:div618\) \(m:stopLoss\) \(m:winG\) \(m:his\) \(m:win-ssq\) \(m:hit-ssq\)`
 
 if [ $? -eq 0 ]
 then
-res=$(guile -q -l $MD/money/money.scm -c "(chdir \"$MD/money\") (print $expr)")
-zenity --info --text="$res"
+    res=$(guile -q -l $MD/scheme/money-guile.scm -c "$expr")
+    zenity --info --text="$res"
 fi
