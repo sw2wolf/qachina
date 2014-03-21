57a719257effa81d698fcf50779075d8db3a12c4
sw2wolf <czsq888@163.com>
2014-03-21 15:06:54 +0800

    misc.

--- db/doc/emacs/init-key.el
+++ db/doc/emacs/init-key.el
@@ -91,12 +91,6 @@
 	  (switch-to-buffer "*CHEZ*")
 	 (async-shell-command "petite /media/D/qachina/db/doc/scheme/money-petite.scm" "*CHEZ*")))
 
-;; (defun jump-run-husk ()
-;;    (interactive)
-;;    (if (get-buffer "*HUSK*")
-;; 	  (switch-to-buffer "*HUSK*")
-;; 	 (async-shell-command "huski -i /media/D/qachina/db/doc/scheme/money-husk.scm" "*HUSK*")))
-
 ;; (remove-if-not (lambda (buff)
 ;; 				 (string-match "\\.p[lm]$" (buffer-name buff))) (buffer-list))
 ;; (some (lambda (buff) (string-match "^*slime-repl" (buffer-name buff))) (buffer-list))
@@ -113,25 +107,21 @@
 (global-set-key (kbd "<f2>") 'find-file-at-point)
 ;(global-set-key (kbd "<f3>") 'describe-char)
 
-;(global-set-key (kbd "<f3>") '(lambda () (interactive) (insert #x3bb)))
 ;√:#x221a π:#x3c0 λ:#x3bb ∑:#x2211 ⊥:#x22a5 ≅:#x2245 ≠:#x2260 ☺:#x263a
 ;⋆:#x22c6 ≅:#x2245
+;(global-set-key (kbd "<f3>") '(lambda () (interactive) (insert #x3bb)))
 
 ; > @wn @src @where @undo @unmtl @pl @package
 ;@djinn turn a type into its corresponding expression
 ;(global-set-key (kbd "<f3>") '(lambda () (interactive) (insert "/msg lambdabot @ty ")))
-(global-set-key (kbd "<f3>") '(lambda ()
-								(interactive)
-								(insert "/msg candide compliment ")))
+
 ;cc printf("%d\n",3);
 ;expand #define WHILE while ( \n #define DO ) { \n #define WEND } \n  int i = 5; WHILE --i DO puts("hi"); WEND
 ;man fork
 ;google [number of results] query
-
-;; (global-set-key (kbd "<f3>") '(lambda () 
-;; 								(interactive)
-;; 								(insert "/msg rudybot ()")
-;; 								(backward-char 1)))
+(global-set-key (kbd "<f3>") '(lambda ()
+								(interactive)
+								(insert "/msg candide man ")))
 
 (global-set-key (kbd "<f4>") 'jump-run-shell)
 (global-set-key (kbd "<f5>") 'jump-run-mew)
@@ -148,7 +138,6 @@
 ;(global-set-key (kbd "<f6>") 'jump-run-clisp)
 
 ;(global-set-key (kbd "<f8>") 'jump-run-chez)
-;(global-set-key (kbd "<f8>") 'jump-run-husk)
 (global-set-key (kbd "<f8>") 'run-scheme)
 
 (global-set-key [(f9)] 'list-bookmarks)
--- db/doc/www/dwb/dwb_screenshot.js
+++ db/doc/www/dwb/dwb_screenshot.js
@@ -15,34 +15,3 @@ function screenshot() {
 }
 bind("ps", screenshot, "screenshot");
 
-function qqwebime() {
-    var q = window.QQWebIME;
-    q ? q.toggle() : (function (d,j) {
-        j = d.createElement("script");
-        j.async = true;
-        j.src = "//ime.qq.com/fcgi-bin/getjs";
-        j.setAttribute("ime-cfg","lt=2");
-        d = d.getElementsByTagName("head")[0];
-        d.insertBefore(j,d.firstChild);
-    }(document));
-}
-bind("Control i", function() {tabs.current.inject(qqwebime) }, OverrideKey.insertMode);
-
-// function test() {
-// 	io.print("hello");
-// }
-// bind("Control t", test, OverrideKey.insertMode);
-
-// function injectable() {
-//     alert("hello");
-// }
-// function callHello() {
-//     tabs.current.inject(injectable);
-// }
-// bind("Control r", callHello, OverrideKey.insertMode);
-
-// function foo() { 
-//     tabs.current.toFoo();
-// }
-// bind(null, foo.debug(script), "foodebug");
-// bind(null, foo, "foo");
