1421458ed195aa96f5a7033575d095fd4ceb848b
sw2wolf <czsq888@163.com>
2014-05-15 15:11:20 +0800

    misc.

--- db/doc/common-lisp/snippet.lisp
+++ db/doc/common-lisp/snippet.lisp
@@ -1,5 +1,38 @@
 
 ;;;;;
+(define-stumpwm-type :password (input prompt)
+  (let ((history *input-history*)
+        (arg (argument-pop input))
+        (fn (symbol-function 'draw-input-bucket)))
+    (unless arg
+      (unwind-protect
+           (setf (symbol-function 'draw-input-bucket)
+                 ;; (lambda (screen prompt input &optional errorp)
+		 (lambda (screen prompt input)
+                   (let ((i (copy-structure input)))
+                     (setf (input-line-string i)
+                           (make-string (length (input-line-string i))
+                                        :initial-element #\*))
+                     (funcall fn screen prompt i)))
+                 arg (read-one-line (current-screen) prompt))
+        (setf (symbol-function 'draw-input-bucket) fn
+              *input-history* history))
+      arg)))
+
+(defmacro sudo-command (name command &key output)
+  (let ((cmd (gensym)))
+    `(defcommand ,name (password) ((:password "Password: "))
+       "Enable StumpWM to execute commands as a privileged user."
+      (let ((,cmd (concat "echo '" password "' | sudo -S " ,command)))
+        ,(if output
+             `(run-prog-collect-output *shell-program* "-c" ,cmd)
+             `(run-prog *shell-program* :args (list "-c" ,cmd) :wait nil))))))
+
+(sudo-command reboot "reboot")
+(sudo-command shutdown "shutdown -h now")
+(sudo-command hibernate "pm-hibernate")
+
+;;;;;
 (ignore-errors
   (with-open-file (r "/dev/urandom" :element-type '(unsigned-byte 32)
                                     :direction :input :if-does-not-exist :error)
