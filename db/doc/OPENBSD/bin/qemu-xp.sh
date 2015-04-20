e3d8caa576858d27a5892a3ef5d2bc9da4e41fa8
sw2wolf <czsq888@163.com>
2015-04-20 15:07:29 +0800

    misc.

--- db/doc/OPENBSD/bin/qemu-xp.sh
+++ db/doc/OPENBSD/bin/qemu-xp.sh
@@ -5,8 +5,8 @@ if [ `pgrep qemu` ]; then
 fi
 qemu-system-i386 -hda /mnt/D/winxp.qcow2 -m 192 -localtime   \
     -net nic,vlan=0,macaddr=52:54:00:12:34:22,model=e1000 -net user \
+	-nographic -redir tcp:3389::3389
     #-usbdevice tablet -vnc 0.0.0.0:1 \
-    -nographic -redir tcp:3389::3389
     #-monitor stdio
 
 #-net nic,vlan=0,macaddr=52:54:00:12:34:22,model=rtl8139 -net user \
--- db/doc/OPENBSD/bin/xp.sh
+++ db/doc/OPENBSD/bin/xp.sh
@@ -1,5 +1,7 @@
 #xset s off # 关闭萤幕保护。
 #xset -dpms # 关闭 DPMS。 
 #xset s 1800 600
-rdesktop -u sw2wolf -p 123 -g 1440x880 -D -K localhost:3389
+#rdesktop -u sw2wolf -p 123 -g 1440x880 -D -K localhost:3389
+rdesktop -u sw2wolf -p 123 -g 800x500 -D -K localhost:3389
+
 #-r sound:remote
